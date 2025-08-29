822082da2a60c2346576f7d9d0e317cef80409be
Erupter20 <boyalharsh1@gmail.com>
2025-08-29 13:11:37 +0530

    day13

---
+++ web/classwork/day13/javascript/script.js
@@ -0,0 +1,38 @@
+
+// var a = 10; 
+// function double(num) {
+// return num = 2; 
+// }
+
+// var res = double(6);
+// console.log(res);
+// var res1 = double(a);
+// console.log(res1); 
+
+// var vikas = (num) => {
+//     return num +2;
+// };
+// var res = vikas(6);
+// console.log(res);
+// var res1 = vikas(a);
+// console.log(res1);
+
+var a = 10;
+const employeeName = "vikas";
+
+// defining value and then printing it.
+let salary = 1234;
+let position = "category lead";
+
+// console.log(employeeNameName);
+// console.log(salary);
+// console.log(position);
+
+// console.table({employeeName, salary, position});
+
+// printing the value first and defining it later
+
+// console.log(age); // printing the value
+// let age = 34; // defining the value (prototypal behaviour, never quitting attitude)
+
+// var is an ultra-luxary value, it allows redefining of var.
